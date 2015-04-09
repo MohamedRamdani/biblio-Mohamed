df99a4e8461b858f9680ee683dc8e7d3e6a9568b
RAYMOND PHILOGENE <praysocool@gmail.com>
2015-04-09 11:32:52 +0200

    Ajout des Utilisateur employe dans la Dao Modification de Test de Base

--- src/Dao/UtilisateurDao.java
+++ src/Dao/UtilisateurDao.java
@@ -4,7 +4,9 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 import metier.Adherent;
+import metier.Employe;
 import metier.EnumStatusExemplaire;
+import metier.EnumcategorieEmploye;
 import metier.Exemplaire;
 import metier.Utilisateur;
 
@@ -15,10 +17,10 @@ public class UtilisateurDao {
 		{
 			new Adherent("DUPONT","Jacques",new GregorianCalendar(1990,Calendar.APRIL,27).getTime(),"Masculin","0682858584",1,"passadh","DUPJ"),
 			new Adherent("JEAN","Paul",(new GregorianCalendar(2015,Calendar.MAY,27).getTime()),"tfs","0785868559",2,"passadh","Jean paul"),
-			new Adherent("EWING","JR",(new GregorianCalendar(2015,Calendar.MAY,27).getTime()),"tfs","0785868559",3,"passadh","ewing jr")
-			/*new Employe("EWING","JR",(new GregorianCalendar(2015,Calendar.MAY,27).getTime()),"tfs","0785868559",3,"passadh","ewing jr"),
-			new Employe("EWING","JR",(new GregorianCalendar(2015,Calendar.MAY,27).getTime()),"tfs","0785868559",3,"passadh","ewing jr"),
-			new Employe("EWING","JR",(new GregorianCalendar(2015,Calendar.MAY,27).getTime()),"tfs","0785868559",3,"passadh","ewing jr"),*/
+			new Adherent("EWING","JR",(new GregorianCalendar(2015,Calendar.MAY,27).getTime()),"tfs","0785868559",3,"passadh","ewing jr"),
+			new Employe("edf","jlde",(new GregorianCalendar(2015,Calendar.APRIL,27).getTime()),"tfs",4,"hdsh","kshkj","fsedf",EnumcategorieEmploye.BIBLIOTHECAIRE),
+			new Employe("edf","jlde",(new GregorianCalendar(2015,Calendar.APRIL,27).getTime()),"tfs",5,"hdsh","kshkj","fsedf",EnumcategorieEmploye.BIBLIOTHECAIRE),
+			new Employe("edf","jlde",(new GregorianCalendar(2015,Calendar.APRIL,27).getTime()),"tfs",6,"hdsh","kshkj","fsedf",EnumcategorieEmploye.BIBLIOTHECAIRE)
 		};
 		
 	
--- src/test/TestDeBase.java
+++ src/test/TestDeBase.java
@@ -12,14 +12,20 @@ public class TestDeBase {
 		UtilisateurDao utilisateur=new UtilisateurDao(); 
 		
 		System.out.println("Demande de deux exemplaires par leur id aux Dao:");
+		System.out.println("************************************************");
 		
 		System.out.println(Dexemp.findByKey(3)); // affichage d'un exemplaire (id=3)
-		System.out.println(Dexemp.findByKey(7)+"\n"); // affichage d'un exemplaire (id=7)
+		System.out.println(Dexemp.findByKey(7)); // affichage d'un exemplaire (id=7)
 		
-		System.out.println("Demande d'un adhérent par leur id aux Dao:");
+		System.out.println("\nDemande d'un adhérent par leur id aux Dao:");
+		System.out.println("******************************************");
 		
-		System.out.println(utilisateur.findByKey(2)); // affichage d'un adhérent (id=2)
+		System.out.println(utilisateur.findByKey(2).getClass().getSimpleName()+":"+utilisateur.findByKey(2));; // affichage d'un adhérent (id=2)
 		
+		
+		System.out.println("\nDemande d'un employé par leur id aux Dao:");
+		System.out.println("*****************************************");
+		System.out.println(utilisateur.findByKey(5).getClass().getSimpleName()+":"+utilisateur.findByKey(5)); // affichage d'un adhérent (id=5)
 		
 		
 
